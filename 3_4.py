46b1c897c6f066b5f6e4191e2735d7b2da062053
Simone Giovannini <simone.giovannini1@stud.unifi.it>
2023-07-18 18:41:17 +0200

    Added QLearning

---
+++ 3_4.py
@@ -0,0 +1,137 @@
+import gymnasium as gym
+import math
+from itertools import count
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+from models.q_network import DQN
+from utils.replay_buffer import ReplayBuffer, Experience
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+                    math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    experiences = memory.sample(BATCH_SIZE)
+    batch = Experience(*zip(*experiences))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                       if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+
+env = gym.make("LunarLander-v2")
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+NUM_EPISODES = 5000
+
+n_actions = env.action_space.n
+state, info = env.reset()
+n_observations = len(state)
+
+writer = SummaryWriter(f'runs/exercise-3_4')
+
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayBuffer(10000)
+
+episode_durations = []
+episodes_rewards = []
+
+steps_done = 0
+
+for i_episode in range(NUM_EPISODES):
+    rewards = []
+    state, info = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        rewards.append(reward)
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
+
+        if terminated:
+            next_state = None
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+        memory.push(state, action, next_state, reward)
+
+        state = next_state
+
+        optimize_model()
+
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * TAU + target_net_state_dict[key] * (1 - TAU)
+        target_net.load_state_dict(target_net_state_dict)
+
+        if done:
+            total_reward = np.sum(rewards)
+            episodes_rewards.append(total_reward)
+            if i_episode % 10 == 0 and i_episode > 0:
+                avg_rew = sum(episodes_rewards[-10:]) / 10
+                writer.add_scalar('Average reward', avg_rew, i_episode)
+                writer.add_scalar('N steps', t + 1, i_episode)
+                print(f'Episode {i_episode}/{NUM_EPISODES} ---- Total reward: {total_reward} ---- Avg reward: {avg_rew}')
+            episode_durations.append(t + 1)
+            break
